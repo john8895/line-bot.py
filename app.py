b6c00a0d4fec8c49a44feee231c7b6d8999f3e53
JohnChen <paiching01@gmail.com>
2020-05-31 11:52:21 +0800

    update

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(event)
     msg = event.message.text
     line_bot_api.reply_message(
         event.reply_token,
